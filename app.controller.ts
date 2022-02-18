aafb05476ce7993983d4abb2dd02525d9d185acb
ReBrandGitHub <99898920+ReBrandGitHub@users.noreply.github.com>
2022-02-18 18:55:16 +0700

    Delete app.controller.ts

--- app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Roles } from './roles/roles.decorator';
-import { Role } from './roles/role.enum';
-
-@Controller()
-export class AppController {
-  constructor() {}
-}
